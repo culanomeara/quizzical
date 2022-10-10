e84d3a43ce3f70e69e182f319c6350589f3db289
Culan O'Meara <culanomeara@gmail.com>
2022-10-10 19:09:48 +0000

    Add question data to json file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
+function getQuestionData()
+
 function displayQuestion() {
     let question = document.getElementById('question')
-    question.innerHTML = "<h1>Hello world!</h1>"
+    question.innerHTML = "<h1></h1>"
 }
