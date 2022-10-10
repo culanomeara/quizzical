6dcac98dbba6755bc618b4bda7ec04e540a962bb
Culan O'Meara <culanomeara@gmail.com>
2022-10-10 20:14:39 +0000

    Add buttons for topic selection on index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let topics = document.getElementsByClassName("topic");
+
+    for (let topic of topics) {
+        topic.addEventListener("click", function() {
+            if (topic.innerHTML === "Trigonometry") {
+                alert("You clicked Trigonometry!");
+            } else {
+                let quizTopic = this.innerHTML;
+                runGame(quizTopic);
+            }
+        });
+    }
+
 function runQuiz() {
 
 }
