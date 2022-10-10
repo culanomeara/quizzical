9be40e5d4c326530b7e75dbbf8aaf844c9c38a11
Culan O'Meara <culanomeara@gmail.com>
2022-10-10 19:23:54 +0000

    Added functions to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
-function getQuestionData()
+function runQuiz() {
+
+}
+function getQuestionData() {
+
+}
 
 function displayQuestion() {
     let question = document.getElementById('question')
     question.innerHTML = "<h1></h1>"
+}
+
+function checkAnswer() {
+
+}
+
+function keepScore() {
+
 }
