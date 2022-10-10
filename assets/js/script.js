481f338a41fb26a93f5322b156ec3c66c44db9dd
Culan O'Meara <culanomeara@gmail.com>
2022-10-10 23:20:00 +0000

    Test function calling using alerts

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,47 +4,47 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
 
-            var quizTopic="";
+            let quizTopic = this.getAttribute("data-type");
 
-            switch (true) {
+            /*switch (true) {
                 // If topic choice is Trig
                 case this.getAttribute("data-type") === "trigonometry":
                     alert("You clicked Trig!");
                     break;
-                // If topic choice is Algebra
+                    // If topic choice is Algebra
                 case this.getAttribute("data-type") === "algebra":
                     alert("You clicked Algebra!");
                     break;
-                // If topic choice is Geometry
+                    // If topic choice is Geometry
                 case this.getAttribute("data-type") === "geometry":
                     alert("You clicked Geometry!");
                     break;
-            }
-            
-            /*if (this.getAttribute("data-type") === "trigonometry") {
-                alert("You clicked Trig!");
-            } else {
-                
             }*/
-            let quizTopic = this.getAttribute("data-type");
-                runQuiz(quizTopic);
+
+            runQuiz(quizTopic);
+
         });
     }
 
 });
 
-
+//This function runQuiz takes in the argument of quizTopic and then using a switch case,
+//calls the relevant topic question function
 function runQuiz(quizTopic) {
-
+    switch (true) {
+        case quizTopic === "trigonometry":
+            alert("Trig");
+        /*displayTrigQuestion();*/
+        case quizTopic === "algebra": 
+        /*displayAlgebraQuestion();*/
+        case quizTopic === "geometry": 
+        /*displayGeometryQuestion();*/
+    }
 }
+
 
 function getQuestionData() {
 
-}
-
-function displayQuestion() {
-    let question = document.getElementById('question')
-    question.innerHTML = "<h1></h1>"
 }
 
 function checkAnswer() {
