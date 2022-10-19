dabae83622f4eeeeeee3a238bf09ecd5951e9cba
Culan O'Meara <culanomeara@gmail.com>
2022-10-19 19:08:00 +0000

    Resolve issue with disabled topic buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@
         currentQtopic = checkTopic();
         document.getElementById('feedback').textContent = "";
         if (currentQtopic == "none") {
-            alert('Please select a topic');
+            alert('Please select a topic for the quiz');
         } else
         if (counter < 4) {
             runQuiz(currentQtopic, counter);
@@ -26,50 +26,7 @@
 
 
 
-    var trigonometryQData = [{
-        topic: "trigonometry",
-        questiontext: "What formula can you use to find the length of a side of a right-angled triangle?",
-        answer: "Pythagoras' Theorem",
-        choices: ["Sine Rule", "Area of a triangle", "Cosine Rule", "Pythagoras' Theorem"]
-    }, {
-        topic: "trigonometry",
-        questiontext: "What method can you use to find the angle in a right-angled triangle?",
-        answer: "SOHCAHTOA",
-        choices: ["Pythagoras' Theorem", "SOHCAHTOA", "Area of a triangle", "Cosine Rule"]
-    }, {
-        topic: "trigonometry",
-        questiontext: "If the Sine Value of an angle is positive, which two quadrants will the angles be located?",
-        answer: "1st and 2nd",
-        choices: ["1st and 2nd", "1st and 3rd", "1st and 4th", "1st Only"]
-    }, {
-        topic: "trigonometry",
-        questiontext: "What angle betwen 0 and 90 degrees has the same Sine and Cosine value?",
-        answer: "30",
-        choices: ["30", "90", "60", "45"]
-    }];
-
-    var algebraQData = [{
-        topic: "algebra",
-        questiontext: "What does HCF stand for?",
-        answer: "Highest Common Factor",
-        choices: ["Hypothetical Common Fact", "Highest Cumulative Form", "Highest Common Factor", "Hypotenuse Change Face"]
-    }, {
-        topic: "algebra",
-        questiontext: "The order of operations in Maths is? ",
-        answer: "BIRDMAS",
-        choices: ["BIRDMAS", "Multiply, Divide, Add, Subtract", "What you need to type into calculator", "As they appear on the page"]
-    }, {
-        topic: "algebra",
-        questiontext: "The -b formula is use to find what?",
-        answer: "Roots of a quadratic",
-        choices: ["Length of a line segment", "Factors", "The square root of a number", "Roots of a quadratic"]
-    }, {
-        topic: "algebra",
-        questiontext: "If you divide any number by 0, what answer do you get?",
-        answer: "Cannot divide a number by 0",
-        choices: ["Cannot divide a number by 0", "0", "1", "the number"]
-    }];
-
+    
     //This function runQuiz takes in the argument of quizTopic and then using a switch case,
     //calls the relevant topic question function
     function runQuiz(quizTopic, counter) {
@@ -87,6 +44,19 @@
                 //  displayGeometryQuestion();
         }
     }
+
+    function checkTopic() {
+        let usertopic = ("None");
+        if (document.getElementById('trigonometry').style.backgroundColor == 'orange') {
+            usertopic = "trigonometry";
+        } else if (document.getElementById('algebra').style.backgroundColor == 'orange') {
+            usertopic = "algebra";
+        } else {
+            alert('Please select a topic');
+            location.reload();
+        }
+        return usertopic;
+    };
 
     function displayTrigQuestion(questionNumber) {
         var i = questionNumber;
@@ -192,15 +162,48 @@
         document.getElementById('ans4').classList.add('disabled');
     }
 
-    function checkTopic() {
-        let usertopic = ("None");
-        if (document.getElementById('trigonometry').style.backgroundColor == 'orange') {
-            usertopic = "trigonometry";
-        } else if (document.getElementById('algebra').style.backgroundColor == 'orange') {
-            usertopic = "algebra";
-        } else {
-            alert('Please select a topic');
-            location.reload();
-        }
-        return usertopic;
-    };
+    var trigonometryQData = [{
+        topic: "trigonometry",
+        questiontext: "What formula can you use to find the length of a side of a right-angled triangle?",
+        answer: "Pythagoras' Theorem",
+        choices: ["Sine Rule", "Area of a triangle", "Cosine Rule", "Pythagoras' Theorem"]
+    }, {
+        topic: "trigonometry",
+        questiontext: "What method can you use to find the angle in a right-angled triangle?",
+        answer: "SOHCAHTOA",
+        choices: ["Pythagoras' Theorem", "SOHCAHTOA", "Area of a triangle", "Cosine Rule"]
+    }, {
+        topic: "trigonometry",
+        questiontext: "If the Sine Value of an angle is positive, which two quadrants will the angles be located?",
+        answer: "1st and 2nd",
+        choices: ["1st and 2nd", "1st and 3rd", "1st and 4th", "1st Only"]
+    }, {
+        topic: "trigonometry",
+        questiontext: "What angle betwen 0 and 90 degrees has the same Sine and Cosine value?",
+        answer: "30",
+        choices: ["30", "90", "60", "45"]
+    }];
+
+    var algebraQData = [{
+        topic: "algebra",
+        questiontext: "What does HCF stand for?",
+        answer: "Highest Common Factor",
+        choices: ["Hypothetical Common Fact", "Highest Cumulative Form", "Highest Common Factor", "Hypotenuse Change Face"]
+    }, {
+        topic: "algebra",
+        questiontext: "The order of operations in Maths is? ",
+        answer: "BIRDMAS",
+        choices: ["BIRDMAS", "Multiply, Divide, Add, Subtract", "What you need to type into calculator", "As they appear on the page"]
+    }, {
+        topic: "algebra",
+        questiontext: "The -b formula is use to find what?",
+        answer: "Roots of a quadratic",
+        choices: ["Length of a line segment", "Factors", "The square root of a number", "Roots of a quadratic"]
+    }, {
+        topic: "algebra",
+        questiontext: "If you divide any number by 0, what answer do you get?",
+        answer: "Cannot divide a number by 0",
+        choices: ["Cannot divide a number by 0", "0", "1", "the number"]
+    }];
+
+    
