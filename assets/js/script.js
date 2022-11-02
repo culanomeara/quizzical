d2e0b3272e18b32eb9b9883a88fc544be6a3c5f2
Culan O'Meara <culanomeara@gmail.com>
2022-11-02 16:45:50 +0000

    Add localstorage for report card data

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,15 @@ let totalQuestion = 4;
 function runQuiz(quizTopic) {
     let element = document.getElementById("next");
     element.addEventListener("click", nextQ);
-    let resetQuiz = document.getElementById("reset");
-    resetQuiz.addEventListener("click", endQuiz);
+    let reset = document.getElementById("reset");
+    reset.addEventListener("click", resetQuiz);
     for (let element of document.getElementsByClassName("topic")) {
         element.hidden = true;
     };
+    document.getElementById('pick').hidden = true;
     document.getElementById('quizContainer').hidden = false;
     qtopic = quizTopic;
-    let displaytopic=qtopic.toUpperCase();
+    let displaytopic = qtopic.toUpperCase();
     document.getElementById('currentTopic').textContent = displaytopic;
     document.getElementById(qtopic).style.backgroundColor = "rgba(255, 192, 203, 0.5)";
     displayQuestion(qtopic, qCounter);
@@ -41,7 +42,7 @@ function nextQ() {
         document.getElementById('finish').hidden = true;
         document.getElementById('finish').innerHTML = 'Next';
         document.getElementById('finish').id = 'next';
-        endQuiz(currentScore);
+        finishQuiz(currentScore, qtopic);
     };
 
 };
@@ -81,7 +82,6 @@ function checkAnswer(userSelect) {
         currentScore++;
         document.getElementById('feedback').textContent = "Well done!";
         ansColor.style.backgroundColor = "rgba(0, 255, 0, 0.5)";
-
     } else {
         document.getElementById('feedback').textContent = `Oops! The correct answer is ${correctanswer}`;
         ansColor.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
@@ -90,32 +90,52 @@ function checkAnswer(userSelect) {
 }
 
 //function that is run once all questions have been answered
-function endQuiz() {
+function finishQuiz() {
+    localStorage.setItem("reportscore", currentScore);
+    localStorage.setItem("reporttopic", qtopic);
+    location.href = 'end.html';
+}
+
+function reportCard() {
+    let element = document.getElementById('grade');
+    let reportScore = localStorage.getItem("reportscore");
+    let reportTopic = localStorage.getItem("reporttopic").toUpperCase();
+    document.getElementById('report').textContent = `You correctly answered ${reportScore} out of 4 Questions in ${reportTopic}`;
+    if (reportScore == 1) {
+        element.textContent = "Your grade is D";
+        element.style.backgroundColor = "rgba(255,192,203,0.5)";
+    } else if (reportScore == 2) {
+        element.textContent = "Your grade is C";
+        element.style.backgroundColor = "rgba(255,170,102,0.5)";
+    } else if (reportScore == 3) {
+        element.textContent = "Your grade is B";
+        element.style.backgroundColor = "rgba(255,255,0,0.5)";
+    } else if (reportScore == 4) {
+        element.textContent = "Your grade is A";
+        element.style.backgroundColor = "rgba(0,128,0,0.5)";
+    } else {
+        element.textContent = "Your grade is E";
+        element.style.backgroundColor = "rgba(255,0,0,0.5)";
+    }
+}
+
+//function that is called when reset button clicked
+function resetQuiz() {
     currentScore = 0;
     qCounter = 1;
     document.getElementById('quizContainer').hidden = true;
     location.reload();
 }
 
-/*function hideTopics(quizTopic) {
-    let topic = quizTopic;
-    if
-    document.getElementById('trigonometry').hidden = true;
-    document.getElementById('algebra').hidden = true;
-    document.getElementById('geometry').hidden = true;
-    document.getElementById('statistics').hidden = true;
-}
-*/
+/* enable/disable answer divs depending on what stage of quiz it's at */
 function enableAnswers() {
-    document.getElementById('ans1').classList.remove('disabled');
-    document.getElementById('ans2').classList.remove('disabled');
-    document.getElementById('ans3').classList.remove('disabled');
-    document.getElementById('ans4').classList.remove('disabled');
+    for (let element of document.getElementsByClassName("answer")) {
+        element.classList.remove('disabled');
+    };
 }
 
 function disableAnswers() {
-    document.getElementById('ans1').classList.add('disabled');
-    document.getElementById('ans2').classList.add('disabled');
-    document.getElementById('ans3').classList.add('disabled');
-    document.getElementById('ans4').classList.add('disabled');
+    for (let element of document.getElementsByClassName("answer")) {
+        element.classList.add('disabled');
+    };
 }
